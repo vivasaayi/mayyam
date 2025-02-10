22a812a96b603797c8903a2ff02c73e3e7846b24
Rajan Panneer Selvam <rajan.india@yahoo.co.in>
2025-02-09 19:01:24 -0500

    Packaged the app with React UI

--- src/main/java/org/poriyiyal/mayyam/userinterface/web/controllers/HealthController.java
+++ src/main/java/org/poriyiyal/mayyam/userinterface/web/controllers/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-    @RequestMapping("/")
+    @RequestMapping("/health")
     String home() {
         return "Hello World!";
     }
